9e8352d36e1aa893e74a4f5d3f2c9a74593a67f6
Jose Garcia <joseluisgarcia31000@gmail.com>
2023-10-21 20:17:37 +0200

    Enregistrement dun nouvel utilisateur avec mdp haché, terminé

--- routes/routesUser.js
+++ routes/routesUser.js
@@ -1,11 +1,11 @@
 // Importation des dépendances
 const express = require('express');
 const router = express.Router();
-const controllersUser= require('../controllers/userControllers');
+const UserControllers= require('../controllers/userControllers');
 
 
 // Creation dun nouveau compte utilisateur
-router.post('/signup', controllersUser.CreateUser);
+router.post('/signup', UserControllers.CreateUser);
 
 
 // Exportation du module
